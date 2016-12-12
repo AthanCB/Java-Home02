2968d9f25714ed62257b1a2f7cc87e5cf81af59c
Georg Dimopoulos <georgdimopoulos@gmail.com>
2016-12-12 14:21:00 +0200

    this instead of rect

--- Home02/src/home02/Card.java
+++ Home02/src/home02/Card.java
@@ -1,31 +1,42 @@
 package home02;
 
-import static com.sun.java.accessibility.util.AWTEventMonitor.addMouseListener;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Polygon;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JComponent;
-import org.w3c.dom.css.CSSPrimitiveValue;
-import org.w3c.dom.css.Rect;
 
 public class Card extends JComponent implements MouseListener {
 
-    boolean stat = true;
+    int repaintCounter = 0;
     final int rectLength = 100;
     private Polygon rect;
     Graphics gp;
+    private boolean statRight = true, statLeft = true;
     private char Character;
     private int Value;
     private Color ColorC;
     private int xCoord, yCoord;
 
+    public void setStatClick(boolean st) {
+        statRight = st;
+    }
+
+    public boolean getStatClick() {
+        return statRight;
+    }
+
+    //empty constructor
+//    public Card() {
+//        Character ='a';
+//        Value = Value;
+//        ColorC = Color.WHITE;
+//        xCoord = 0;
+//        yCoord = 0;
+//    }
     public Card(char Character, int Value, Color color, int x, int y) {
         this.Character = Character;
         this.Value = Value;
@@ -41,6 +52,22 @@ public class Card extends JComponent implements MouseListener {
         addMouseListener(this);
     }
 
+    public void setLeftClick(boolean stat) {
+		statLeft = stat;
+	}
+
+	public boolean getLeftClick() {
+		return statLeft;
+	}
+
+	public void setRightClick(boolean st) {
+		statRight = st;
+	}
+
+	public boolean getRightClick() {
+		return statRight;
+	}
+    
     public void setRect(Polygon rect) {
         this.rect = rect;
     }
@@ -54,7 +81,6 @@ public class Card extends JComponent implements MouseListener {
         super.paintComponent(g);
         String Letter = "" + Character;
         String valueS = "" + Value;
-
         g.setColor(Color.BLACK);
         g.drawRect(xCoord, yCoord, rectLength, rectLength); // rectLength=100
         g.setColor(ColorC);
@@ -65,27 +91,42 @@ public class Card extends JComponent implements MouseListener {
         g.setFont(new Font("Courier", Font.BOLD, 12));
         g.drawString(valueS, 80 + xCoord, 80 + yCoord);
         g.drawPolygon(rect);
-        if(stat==false) {
-            g.setColor(Color.YELLOW);
-            g.fillRect(xCoord, yCoord, rectLength, rectLength);
-        } 
-       
     }
-    
+
     @Override
-    public void repaint(){
-        
+    public void repaint() {
+        System.out.println("repaint: " + repaintCounter);
+        repaintCounter++;
     }
+
+    public void Myrepaint(Graphics g, MouseEvent me) {
+        System.out.println("\nMyrepaint: ");
+//		if(rect.contains(getMousePosition()))
+//			System.out.println("WORKS");
+        if (statLeft == false) {// left button, chosen letter
+            g.setColor(Color.yellow);
+            g.fillRect(xCoord, yCoord, rectLength, rectLength);
+        }
+        if (statRight == false) { // right button
+            
+        }
+    }
+
     @Override
     public void mouseClicked(MouseEvent me) {
         Point point = me.getPoint();
-        if (rect.contains(point)) {
+        System.out.println("POINT: " + point);
+        if (this.contains(point)) {
             System.out.println("Contains");
             if (me.getButton() == MouseEvent.BUTTON1) {
-                stat = false;
-                System.out.println("fdwwwg");
+                setLeftClick(false);// gia na allaksei to xrwma tou tetragwnou               
+                System.out.println("left");
+                Myrepaint(this.getGraphics(),me);
+            } else if (me.getButton() == MouseEvent.BUTTON3) {
+                setRightClick(false);
+                System.out.println("right");
+                Myrepaint(this.getGraphics(),me);
             }
-            
         }
     }
 
